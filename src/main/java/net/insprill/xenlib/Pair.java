f8d6d35349c8547debf8ae2520a271fe6253790f
Pierce Thompson <insprill@gmail.com>
2022-01-11 23:20:06 -0500

    Add no args constructor to Pair class

--- src/main/java/net/insprill/xenlib/Pair.java
+++ src/main/java/net/insprill/xenlib/Pair.java
@@ -2,11 +2,13 @@ package net.insprill.xenlib;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
 
 @AllArgsConstructor
+@NoArgsConstructor
 public class Pair<K, V> implements Serializable {
 
     private static final long serialVersionUID = 1L;
