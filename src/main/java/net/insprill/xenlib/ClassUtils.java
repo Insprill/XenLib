abaaaeb036d6cf3cf58c0fd07ac4736c28f41fd1
Pierce Thompson <insprill@gmail.com>
2022-05-20 21:45:13 -0400

    Add deep parameter to ClassUtils#getImplementingClasses

--- src/main/java/net/insprill/xenlib/ClassUtils.java
+++ src/main/java/net/insprill/xenlib/ClassUtils.java
@@ -10,13 +10,34 @@ import java.util.stream.Collectors;
 @UtilityClass
 public class ClassUtils {
 
+    /**
+     * Gets all classes in the package that implement the given interface. Doesn't include sub-packages.
+     *
+     * @param packageName     Package to search. CaSe-SeNsItIvE.
+     * @param targetInterface Interface to search for.
+     * @param <T>             Type of the interface.
+     * @return Set of classes that implement the interface.
+     */
+    public <T> Set<Class<T>> getImplementingClasses(String packageName, Class<T> targetInterface) {
+        return getImplementingClasses(packageName, targetInterface, false);
+    }
+
+    /**
+     * Gets all classes in the package that implement the given interface.
+     *
+     * @param packageName     Package to search. CaSe-SeNsItIvE.
+     * @param targetInterface Interface to search for.
+     * @param deep            Whether to search sub-packages.
+     * @param <T>             Type of the interface.
+     * @return Set of classes that implement the interface.
+     */
     @SneakyThrows
     @SuppressWarnings({"UnstableApiUsage", "unchecked"})
-    public <T> Set<Class<T>> getImplementingClasses(String packageName, Class<T> targetInterface) {
+    public <T> Set<Class<T>> getImplementingClasses(String packageName, Class<T> targetInterface, boolean deep) {
         return ClassPath.from(XenLib.getPlugin().getClass().getClassLoader())
                 .getAllClasses()
                 .parallelStream()
-                .filter(clazz -> clazz.getPackageName().equalsIgnoreCase(packageName))
+                .filter(info -> (deep && info.getPackageName().startsWith(packageName)) || (!deep && info.getPackageName().equals(packageName)))
                 .map(ClassPath.ClassInfo::load)
                 .filter(targetInterface::isAssignableFrom)
                 .filter(clazz -> clazz != targetInterface)
